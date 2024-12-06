844acdc191586afcb88720b7ff163c8f8d72bf4f
github-action-benchmark <github@users.noreply.github.com>
2024-12-06 05:17:33 +0000

    add macOS (customSmallerIsBetter) benchmark result for 926e5be05e9f4a2df5b26a8ec3084e4c2053a048

---
+++ dev/bench/data.js
@@ -0,0 +1,57 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1733462253217,
+  "repoUrl": "https://github.com/tolkonepiu/zdotdir",
+  "entries": {
+    "macOS": [
+      {
+        "commit": {
+          "author": {
+            "email": "popoffpavel@yandex.ru",
+            "name": "Pavel Popov",
+            "username": "tolkonepiu"
+          },
+          "committer": {
+            "email": "popoffpavel@yandex.ru",
+            "name": "Pavel Popov",
+            "username": "tolkonepiu"
+          },
+          "distinct": true,
+          "id": "926e5be05e9f4a2df5b26a8ec3084e4c2053a048",
+          "message": "Added permissions for the GitHub Actions workflow in `zsh-benchmark.yml` to enable deployments and content updates",
+          "timestamp": "2024-12-06T13:15:57+08:00",
+          "tree_id": "5a08c0013f87ba9f19a39c1785e23e153642a9a3",
+          "url": "https://github.com/tolkonepiu/zdotdir/commit/926e5be05e9f4a2df5b26a8ec3084e4c2053a048"
+        },
+        "date": 1733462251971,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "first_prompt_lag_ms",
+            "value": 31.92,
+            "unit": "ms"
+          },
+          {
+            "name": "first_command_lag_ms",
+            "value": 32.034,
+            "unit": "ms"
+          },
+          {
+            "name": "command_lag_ms",
+            "value": 0.057,
+            "unit": "ms"
+          },
+          {
+            "name": "input_lag_ms",
+            "value": 0.105,
+            "unit": "ms"
+          },
+          {
+            "name": "exit_time_ms",
+            "value": 28.661,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
