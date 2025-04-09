c0401c8c491f1fe6b788d83f312bd2f28f0e501e
Pavel Popov <me@popov.wtf>
2025-04-09 14:00:43 +0700

    Add SKIP_UPDATE parameters in zdotdir function

--- .zshrc.d/zdotdir.zsh
+++ .zshrc.d/zdotdir.zsh
@@ -4,43 +4,48 @@
 zdotdir() {
     # Check if the first argument is "update"
     if [[ "$1" == "update" ]]; then
-        # Ensure git is installed
-        if ! command -v git >/dev/null; then
-            echo "Error: git is not installed." >&2
-            return 1
-        fi
-
-        # Check if $ZDOTDIR is set and navigate to it
-        if [[ -z "$ZDOTDIR" ]]; then
-            echo "Error: ZDOTDIR is not set." >&2
-            return 1
-        fi
-        cd "$ZDOTDIR" || {
-            echo "Error: Failed to cd into $ZDOTDIR"
-            return 1
-        }
-
-        # Perform a git pull to update the repository
-        echo "Updating zdotdir repository..."
-        git pull >/dev/null || {
-            echo "Error: Failed to update zdotdir repository."
-            return 1
-        }
-
-        # Source Antidote if it's installed in $ZDOTDIR or $HOME; otherwise, skip loading.
-        [[ ! -f ${ZDOTDIR:-$HOME}/.antidote/antidote.zsh ]] || source ${ZDOTDIR:-$HOME}/.antidote/antidote.zsh
-
-        # Check if antidote is installed
-        if command -v antidote >/dev/null; then
-            echo "Updating antidote bundles..."
-            # Update antidote bundles
-            antidote update >/dev/null || {
-                echo "Error: Failed to update antidote bundles."
+        # Check if we should skip git pull
+        if [[ -z "$ZDOTDIR_SKIP_UPDATE" ]]; then
+            # Ensure git is installed
+            if ! command -v git >/dev/null; then
+                echo "Error: git is not installed." >&2
+                return 1
+            fi
+            
+            # Check if $ZDOTDIR is set and navigate to it
+            if [[ -z "$ZDOTDIR" ]]; then
+                echo "Error: ZDOTDIR is not set." >&2
+                return 1
+            fi
+            cd "$ZDOTDIR" || {
+                echo "Error: Failed to cd into $ZDOTDIR"
                 return 1
             }
-        else
-            # If antidote is not installed, notify the user
-            echo "Antidote is not installed. Skipping antidote update."
+            
+            # Perform a git pull to update the repository
+            echo "Updating zdotdir repository..."
+            git pull >/dev/null || {
+                echo "Error: Failed to update zdotdir repository."
+                return 1
+            }
+        fi
+        
+        if [[ -z "$ANTIDOTE_SKIP_UPDATE" ]]; then
+            # Source Antidote if it's installed in $ZDOTDIR or $HOME; otherwise, skip loading.
+            [[ ! -f ${ANTIDOTE_PATH}/antidote.zsh ]] || source ${ANTIDOTE_PATH}/antidote.zsh
+            
+            # Check if antidote is installed
+            if command -v antidote >/dev/null; then
+                echo "Updating antidote bundles..."
+                # Update antidote bundles
+                antidote update >/dev/null || {
+                    echo "Error: Failed to update antidote bundles."
+                    return 1
+                }
+            else
+                # If antidote is not installed, notify the user
+                echo "Antidote is not installed. Skipping antidote update."
+            fi
         fi
         echo "zdotdir update completed successfully."
     else
