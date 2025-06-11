96d186f64bd18f2b9da08d058558169227e6e671
Pavel Popov <me@popov.wtf>
2025-06-11 16:37:12 +0800

    Improve find aliases with fd utility integration

--- .zshrc.d/aliases.zsh
+++ .zshrc.d/aliases.zsh
@@ -14,9 +14,13 @@ alias la='ls -lAh'
 # fix common typos
 alias quit='exit'
 
-# find
-alias fd='find . -type d -name'
-alias ff='find . -type f -name'
+# find - use fd utility if available, otherwise fallback to find aliases
+if command -v fd >/dev/null 2>&1; then
+    alias ff='fd -t file'
+elif
+    alias fd='find . -type d -name'
+    alias ff='find . -type f -name'
+fi
 
 # Use zoxide instead of cd if it's installed
 if has-zoxide; then
