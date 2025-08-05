ffdea9f594f60431c4195a0b5feba090ec138e3c
Pavel Popov <me@popov.wtf>
2025-08-06 03:07:30 +0800

    Revert "feat: improve zsh configuration structure and aliases"
    This reverts commit 43d3954dfde938399a83f0149d3aa7e12d87dc5b.

--- .zshrc.d/aliases.zsh
+++ .zshrc.d/aliases.zsh
@@ -12,14 +12,11 @@ if (( $+commands[eza] )); then
     alias ll='eza -l'
     alias la='eza -a'
     alias lla='eza -la'
-    alias ldot='eza -ld .*'
     alias lt='eza --tree'
 else
     alias ls='ls --color=auto'
     alias ll='ls -lh'
-    alias la='ls -a'
-    alias lla='ls -lAh'
-    alias ldot='ls -ld .*'
+    alias la='ls -lAh'
 fi
 
 # fix common typos
--- .zshrc.d/history-substring-search.zsh
+++ .zshrc.d/history-substring-search.zsh
@@ -1,2 +1,2 @@
-bindkey -M vicmd 'k' history-substring-search-up
-bindkey -M vicmd 'j' history-substring-search-down
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
