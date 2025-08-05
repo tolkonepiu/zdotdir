43d3954dfde938399a83f0149d3aa7e12d87dc5b
Pavel Popov <me@popov.wtf>
2025-08-06 01:13:30 +0800

    feat: improve zsh configuration structure and aliases
    - Reorganize .zsh_plugins.txt with better categorization and comments
    - Add keybindings and history plugins from belak/zsh-utils
    - Add getantidote/use-omz for OMZ compatibility
    - Add tailscale plugin and improve defer loading
    - Fix aliases.zsh with consistent ldot alias for both eza/ls
    - Switch history-substring-search to vi-mode bindings (j/k)
    - Improve overall performance with proper defer attributes

--- .zshrc.d/aliases.zsh
+++ .zshrc.d/aliases.zsh
@@ -12,11 +12,14 @@ if (( $+commands[eza] )); then
     alias ll='eza -l'
     alias la='eza -a'
     alias lla='eza -la'
+    alias ldot='eza -ld .*'
     alias lt='eza --tree'
 else
     alias ls='ls --color=auto'
     alias ll='ls -lh'
-    alias la='ls -lAh'
+    alias la='ls -a'
+    alias lla='ls -lAh'
+    alias ldot='ls -ld .*'
 fi
 
 # fix common typos
