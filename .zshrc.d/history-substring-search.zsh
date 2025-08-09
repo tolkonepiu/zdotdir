9a4955bb76107562e90f34391caaf5530c37468a
Pavel Popov <me@popov.wtf>
2025-08-09 13:07:09 +0800

    fix(zsh): use  in bindkey loop to avoid 'no such keymap ''

--- .zshrc.d/history-substring-search.zsh
+++ .zshrc.d/history-substring-search.zsh
@@ -1,3 +1,4 @@
+#!/bin/zsh
 [[ -v terminfo ]] || zmodload zsh/terminfo
 
 # Vi
@@ -6,7 +7,7 @@ bindkey -M vicmd "j" history-substring-search-down
 
 # Emacs and Vi
 for _keymap in 'emacs' 'viins'; do
-  bindkey -M "$keymap" "$terminfo[kcuu1]" history-substring-search-up
-  bindkey -M "$keymap" "$terminfo[kcud1]" history-substring-search-down
+  bindkey -M "$_keymap" "$terminfo[kcuu1]" history-substring-search-up
+  bindkey -M "$_keymap" "$terminfo[kcud1]" history-substring-search-down
 done
 unset _keymap
